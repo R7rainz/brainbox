440f4fd60f2aeff7db4ef9d1fe3db54e8c5341e1
Ronak Kamboj <ronakkamboj26@gmail.com>
2024-11-20 21:32:30 +0530

    unkown commit

--- app/_components/Logo.jsx
+++ app/_components/Logo.jsx
@@ -4,12 +4,12 @@ import React from 'react'
 
 function Logo() {
   return (
-    <Link href="/dashboard" className="flex items-center gap-2 ml-4"> {/* Added margin-left to the Link */}
+    <Link href="/dashboard" className="flex items-center gap-2 ml-4">
       <Image
         src="/logo.png" // Ensure the logo is inside the public directory
         alt="BrainBox Logo"
-        width={30}
-        height={30}
+        width={70} // Increased size to 70px
+        height={70} // Increased size to 70px
         className="object-contain"
       />
       <h2 className="font-bold text-xl text-white">BrainBox</h2>
